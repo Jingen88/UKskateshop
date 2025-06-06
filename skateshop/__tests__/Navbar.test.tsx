9c64abd95947fa1960823b1d4a0260e657691c03
Tamas H <157651056+Jingen88@users.noreply.github.com>
2025-06-06 21:19:10 +0100

    ok

---
+++ skateshop/__tests__/Navbar.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import Navbar from '../components/Navbar';
+
+test('renders Navbar component', () => {
+    render(<Navbar />);
+    const linkElement = screen.getByText(/home/i);
+    expect(linkElement).toBeInTheDocument();
+});
