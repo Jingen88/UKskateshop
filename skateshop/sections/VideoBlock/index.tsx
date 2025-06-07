537d429d143468d40bf5c418c25e830f4323435c
Tamas H <157651056+Jingen88@users.noreply.github.com>
2025-06-07 19:57:20 +0100

    ok

--- skateshop/sections/VideoBlock/index.tsx
+++ skateshop/sections/VideoBlock/index.tsx
@@ -1,55 +1,19 @@
 import { Bounded } from "@/components/Bounded";
-import { Content, isFilled } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
 import { LazyYouTubePlayer } from "./LazyYouTubePlayer";
-import clsx from "clsx";
 import Image from "next/image";
 
 const MASK_CLASSES =
   "[mask-image:url(/video-mask.png)] [mask-mode:alpha] [mask-position:center_center] [mask-repeat:no-repeat] [mask-size:100%_auto]";
 
-/**
- * Props for `VideoBlock`.
- */
-export type VideoBlockProps = SliceComponentProps<Content.VideoBlockSlice>;
-
-/**
- * Component for "VideoBlock" Slices.
- */
-const VideoBlock = ({ slice }: VideoBlockProps): JSX.Element => {
+const VideoBlock = ({ youTubeID }: { youTubeID: string }) => {
   return (
     <Bounded
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
+      as="section"
       className="bg-texture bg-zinc-900"
     >
-      <h2 className="sr-only">Video Reel</h2>
       <div className="relative aspect-video">
-        {/* Masks */}
-        <div
-          className={clsx(
-            MASK_CLASSES,
-            "bg-brand-lime absolute inset-0 ~translate-x-2/3 ~translate-y-2/3"
-          )}
-        />
-        <div
-          className={clsx(
-            MASK_CLASSES,
-            "bg-white absolute inset-0 ~translate-x-1/3 ~translate-y-1/2"
-          )}
-        />
-        <div
-          className={clsx(
-            MASK_CLASSES,
-            "bg-white absolute inset-0 ~translate-x-1/2 ~-translate-y-1/3"
-          )}
-        />
-        {/* Video */}
-        <div className={clsx(MASK_CLASSES, "relative h-full")}>
-          {isFilled.keyText(slice.primary.youtube_video_id) ? (
-            <LazyYouTubePlayer youTubeID={slice.primary.youtube_video_id} />
-          ) : null}
-          {/* Texture overlay */}
+        <div className={MASK_CLASSES + " relative h-full"}>
+          {youTubeID ? <LazyYouTubePlayer youTubeID={youTubeID} /> : null}
           <Image
             src="/image-texture.png"
             alt=""
